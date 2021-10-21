d50693a75381f6a009a1e1ea03f9a6fad2c7be36
tpdms <tpdms0159@naver.com>
2021-10-22 02:16:31 +0900

    window camera

--- BracketsDemoApp/Functions/TakePhoto.js
+++ BracketsDemoApp/Functions/TakePhoto.js
@@ -16,21 +16,41 @@ import Button from '../components/Button';
 import styled from 'styled-components';
 import ShopStepThree2 from '../pages/ShopStepThree2';
 import { NavigationContainer } from '@react-navigation/native';
+import { stubArray } from 'lodash';
 
 
 
+
+const CameraButton = styled.View`
+  flex : 1;
+  align-items:center;
+  background-color : black;
+`;
 const Take = styled.View`
-  height : 100px;
-  width :100px;
+  height : 90px;
+  width :90px;
   border-radius : 50px;
   border : solid 10px black;
   background-color : white;
-  margin-top : 10px;
+  margin-bottom : 15px;
 `;
 
-const Touch  = styled.TouchableOpacity``;
+const Touch  = styled.TouchableOpacity`
 
+`;
 
+const Label = styled.Text`
+
+  text-align : center;
+  color : white;
+  font-size : 12px;
+
+`;
+const PressButton = styled.View`
+  flex : 1;
+  flex-direction : row;
+  margin-top : 200px;
+`;
 export default class TakePhoto extends Component {
   
 
@@ -49,8 +69,11 @@ export default class TakePhoto extends Component {
       console.log (imgUri);
       store.dispatch({type:'PHOTO',photo: imgUri});
       console.log(store.getState().picture);
+
+      
       
       if (route.params.key === 'ShopStepThree2'){
+        console.log (route.params.key);
         this.props.navigation.replace(route.params.key);
       }
 
@@ -64,32 +87,32 @@ export default class TakePhoto extends Component {
     const {route}=this.props;
     console.log(route.params.key);
 
-    const [read,setRead] = " ";
-
-    if (route.params.key === 'ShopStepThree2') {
-      setRead("제품을 평평한 곳에 놓고, 전면 또는 후면이 모두 보이도록");
-    }
     
     return (
-      <Container>
-        <RNCamera
-         ref={ref => {
-          this.camera = ref;
-        }}
-          style={{width: 200, height: 200}}
-          type={RNCamera.Constants.Type.back}
-          captureAudio={false}
-        />
+      <CameraButton>
         <View>
-          <Text >{read}</Text>
-          </View>
+          <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+            style={{width: 500, height: 150}}
+            type={RNCamera.Constants.Type.back}
+            captureAudio={false}
+          />
+        
+          <Label >제품을 평평한 곳에 놓고</Label>
+          <Label >전면 또는 후면이 모두 보이도록</Label>
+          <Label >제품의 전체 사진을 촬영하세요</Label>
+        </View>
     
-      <Touch onPress = { this.onSuccess.bind(this)}>
-      
-        <Take/>
-      </Touch>
+        <PressButton>
+          <Touch onPress = { this.onSuccess.bind(this)}>
+            <Take/>
+          </Touch>
+      </PressButton>
 
-      </Container>
+      </CameraButton>
+
       
     );
   }
