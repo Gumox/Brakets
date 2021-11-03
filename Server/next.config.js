2d1df6ad371305406e22d8c33b3ad9594493e12f
ahrimy <dlsdjdkfla@gmail.com>
2021-11-04 03:18:06 +0900

    환경변수 설정

--- Server/next.config.js
+++ Server/next.config.js
@@ -1,21 +1,24 @@
 module.exports = {
-  	reactStrictMode: true,
-	/*
+  reactStrictMode: true,
+  /*
 	node: {
                 fs: 'empty',
                 net: 'empty',
                 tls: 'empty',
         },
 	*/
-	env: {
-	'MYSQL_HOST': 'localhost',
-	'MYSQL_PORT': '3306',
-	'MYSQL_DATABASE': 'Adidas_01',
-	'MYSQL_USER': 'admin',
-	'MYSQL_PASSWORD': 'qmfozlcm21',
-	},
+  env: {
+    MYSQL_HOST: process.env.NEXT_PUBLIC_MYSQL_HOST,
+    MYSQL_PORT: "3306",
+    MYSQL_DATABASE: "Adidas_01",
+    MYSQL_USER: "admin",
+    MYSQL_PASSWORD: "qmfozlcm21",
+	  KAKAO_CLIENT_ID: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID,
+	  KAKAO_REDIRECT_URI: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI,
+    JWT_SECRET_KEY: "bracketssecretkey"
+  },
 
-	/*
+  /*
 	init: function () {
 		return mysql.createConnection(db_info);
     	},
@@ -27,22 +30,20 @@ module.exports = {
     	},
 	*/
 
-	webpack5: true,
-	webpack: (config) => {
-    		config.resolve.fallback = { 
-			fs: false,
-			net: false,
-                	tls: false,
-		
-		};
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      tls: false,
+    };
 
-    	return config;
-  	},
+    return config;
+  },
 
-	resolve: {
-      		fallback: {
-        	crypto: require.resolve('crypto-browserify')
-      		}
-    	},
-
-}
+  resolve: {
+    fallback: {
+      crypto: require.resolve("crypto-browserify"),
+    },
+  },
+};
