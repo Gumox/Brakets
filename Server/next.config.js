51e08eff84b95b9bd9908bd953b1df0a8d049617
ahrimy <dlsdjdkfla@gmail.com>
2021-11-05 16:50:36 +0900

    Server 설정 추가

--- Server/next.config.js
+++ Server/next.config.js
@@ -1,48 +1,50 @@
 module.exports = {
-  	reactStrictMode: true,
+	reactStrictMode: true,
 	/*
-	node: {
-                fs: 'empty',
-                net: 'empty',
-                tls: 'empty',
-        },
-	*/
+	  node: {
+				  fs: 'empty',
+				  net: 'empty',
+				  tls: 'empty',
+		  },
+	  */
 	env: {
-	'MYSQL_HOST': 'localhost',
-	'MYSQL_PORT': '3306',
-	'MYSQL_DATABASE': 'Adidas_01',
-	'MYSQL_USER': 'admin',
-	'MYSQL_PASSWORD': 'qmfozlcm21',
+	  MYSQL_HOST: process.env.NEXT_PUBLIC_MYSQL_HOST,
+	  MYSQL_PORT: "3306",
+	  MYSQL_DATABASE: "Adidas_01",
+	  MYSQL_USER: "admin",
+	  MYSQL_PASSWORD: "qmfozlcm21",
+		KAKAO_CLIENT_ID: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID,
+		KAKAO_REDIRECT_URI: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI,
+	  JWT_SECRET_KEY: "bracketssecretkey",
+	  API_URL: process.env.NEXT_PUBLIC_API_URL,
 	},
-
+  
 	/*
-	init: function () {
-		return mysql.createConnection(db_info);
-    	},
-    	connect: function(conn) {
-		conn.connect(function(err) {
-	    	if(err) console.error('mysql connection error : ' + err);
-	    	else console.log('mysql is connected successfully!');
-		});
-    	},
-	*/
-
+	  init: function () {
+		  return mysql.createConnection(db_info);
+		  },
+		  connect: function(conn) {
+		  conn.connect(function(err) {
+			  if(err) console.error('mysql connection error : ' + err);
+			  else console.log('mysql is connected successfully!');
+		  });
+		  },
+	  */
+  
 	webpack5: true,
 	webpack: (config) => {
-    		config.resolve.fallback = { 
-			fs: false,
-			net: false,
-                	tls: false,
-		
-		};
-
-    	return config;
-  	},
-
+	  config.resolve.fallback = {
+		fs: false,
+		net: false,
+		tls: false,
+	  };
+  
+	  return config;
+	},
+  
 	resolve: {
-      		fallback: {
-        	crypto: require.resolve('crypto-browserify')
-      		}
-    	},
-
-}
+	  fallback: {
+		crypto: require.resolve("crypto-browserify"),
+	  },
+	},
+  };
--- Server/pages/_app.js
+++ Server/pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>수선 OK</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
