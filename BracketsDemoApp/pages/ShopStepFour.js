c0d8a0a7f305fd47614297e18e3272f489ab0d2a
Gumox <fodhs1000@naver.com>
2021-11-05 14:02:02 +0900

    Update ShopStepFour.js

--- BracketsDemoApp/pages/ShopStepFour.js
+++ BracketsDemoApp/pages/ShopStepFour.js
@@ -3,12 +3,10 @@ import Container from '../components/Container';
 import Button from '../components/Button';
 import styled from 'styled-components/native';
 import CenterText from '../components/CenterText';
-import _, { concat, constant } from 'lodash';
+import _ from 'lodash';
 import Contents from '../components/Contents';
 import StateBarSolid from '../components/StateBarSolid';
 import StateBarVoid from '../components/StateBarVoid';
-import Picker from 'react-native-picker-select';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const Label = styled.Text`
     font-size: 15px;
@@ -42,46 +40,26 @@ const GrayText = styled.Text`
     color:#858585;
 `;
 const TopStateView = styled.View`
-    flex:1;
     flex-direction: row;
-    padding-bottom:24px;
+    padding:24px;
     justify-content: center;
 `;
-const DropBackground= styled.View`
-    width: 220px;
-    background-color:#d6d6d6;
-    border-radius:10px;
-    font-color:#ffffff
-`;
-// 구조 분해 할당, Destructuring Assignment
 
+// 구조 분해 할당, Destructuring Assignment
 function ShopStepFour( { navigation } ) {
-    const List=[
-        { label: '맥가이버', value: '맥가이버' },
-        { label: '로보캅', value: '로보캅' },
-        { label: '폴리', value: '폴리' }];
     
-    const itemList=['맥가이버2','로보캅2','폴리2'];
-    const List2 =[];
-    if (itemList !==null){
-        itemList.map(x => List2.push({label:x,value:x}));
-        
-    }
-    const List0 = [{label:List2[0].label,value:List2[0].value}]
     return (
-        
         <Container>
-            
+            <TopStateView><StateBarSolid/><StateBarSolid/><StateBarSolid/><StateBarSolid/><StateBarVoid/></TopStateView>
             <CenterText>
-                <TopStateView><StateBarSolid/><StateBarSolid/><StateBarSolid/><StateBarSolid/><StateBarVoid/></TopStateView>
-                <RegistText>서비스 카드 확인</RegistText>
+                
+                <RegistText>수선증 확인</RegistText>
                 <GrayText>및</GrayText>
-                <RegistText>서비스 카드 스캔</RegistText>
+                <RegistText>수선증 스캔</RegistText>
                 
             </CenterText>
             
-
-            <BlueText>서비스 카드의 바코드</BlueText>
+            <BlueText>수선증 바코드</BlueText>
             <GrayText>를 스캔하세요</GrayText>
 
             <Button onPress={ ()=> navigation.navigate( 'ScanScreen',{key:'ShopStepFour2'} ) }>
