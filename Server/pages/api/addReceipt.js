86d3ea3a2faa9a42083f7c199bf02263b5395341
yeos60490 <yeos60490@gmail.com>
2021-11-06 18:02:00 +0900

    api - add Receipt step 2

---
+++ Server/pages/api/addReceipt.js
@@ -0,0 +1,45 @@
+import excuteQuery from './db';
+
+
+export async function addReceipt(store, staff, customer, pid, pcode, subsititude) {
+    try {
+        const result = await excuteQuery({
+            query: "INSERT INTO `receipt`(`store_id`, `staff_id`, `customer_id`, `product_id`, `product_code`, `subsititude`) VALUES (?,?,?,?,?,?)",
+            values: [store, staff, customer, pid, pcode, subsititude]
+        });
+
+	//console.log("id"+ result['insertId']);
+        return result;
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export default (req, res) => {
+  if (req.method === "POST") {
+    console.log("req"+ req.body);
+    var store = req.body.store; 
+    var staff = req.body.staff; 
+    var customer = req.body.customer; 
+    var pid = req.body.pid; 
+    var pcode = req.body.pcode; 
+    var subsititude = req.body.subsititude;
+
+    addReceipt(store, staff, customer, pid, pcode, subsititude).then((body) => {
+
+	if(body['affectedRows']){
+                console.log("add Receipt (step 2)");
+                res.statusCode = 200;
+                res.json({receipt_id: body['insertId']});
+        }
+        else{
+                console.log("add Receipt (step2) failed");
+                res.status(400).send();
+        }
+
+    });
+  }
+};
+
