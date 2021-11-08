0adf580445dbb8745eba73fb452bb1dff7502c50
ahrimy <dlsdjdkfla@gmail.com>
2021-11-08 21:50:47 +0900

    menu, dummy 데이터 추가

---
+++ Server/constants/dummy.js
@@ -0,0 +1,62 @@
+export const OPTIONS = [
+    {
+      key: 1,
+      value: 1,
+      text: "First Option",
+    },
+    {
+      key: 2,
+      value: 2,
+      text: "Second Option",
+    },
+    {
+      key: 3,
+      value: 3,
+      text: "Third Option",
+    },
+  ];
+
+export const SEASON_OPTIONS = [
+  {
+    key: '2021fw',
+    value: '2021fw',
+    text: "21 F/W",
+  },
+  {
+    key: '2021ss',
+    value: '2021ss',
+    text: "21 S/S",
+  },
+]
+export const STORE_OPTIONS = [
+  {
+    key: '2',
+    value: '2',
+    text: "아디다스 오리지널스 홍대 직영점",
+  },
+]
+
+
+  export const PRODUCT = {
+    season: "21f/w",
+    style: "?",
+    degree: "1",
+    color: "Black",
+    size: "S",
+    barcode: "AAAA0001",
+    release_date: "2020-10-10",
+    substitute_code: "AAAA0002",
+  }
+  export const STORE = {
+    store_id: "2",
+  }
+  export const CUSTOMER = {
+    customer_name: "USERAA",
+    customer_contact: "000-0000-0000",
+  }
+  export const RECEIPT = {
+    receipt_type: "수선",
+    receipt_date: "2021-01-23",
+    due_date: "2021-02-10",
+    receipt_code: "7099433"
+  }
---
+++ Server/constants/menu.js
@@ -0,0 +1,32 @@
+const MENUS = [
+    {
+      title: "수선접수/처리",
+      link: "/admin/reception",
+    },
+    {
+      title: "하자반품",
+      link: "/admin/return",
+    },
+    {
+      title: "업체클레임",
+      link: "/admin/claim",
+    },
+    {
+      title: "유상수선",
+      link: "/admin/paid-repair",
+    },
+    {
+      title: "현금영수증",
+      link: "/admin/cash-receipt",
+    },
+    {
+      title: "SMS 전송",
+      link: "/admin/sms",
+    },
+    {
+      title: "SMS 결과",
+      link: "/admin/sms-result",
+    },
+  ];
+
+  export default MENUS;
