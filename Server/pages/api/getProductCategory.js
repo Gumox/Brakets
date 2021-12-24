d1a49b2afec3f9494d5459765164659419738eed
ahrimy <dlsdjdkfla@gmail.com>
2021-12-24 15:42:02 +0900

    product category 조회 api 수정, 수선처 정보 조회 api 추가

--- Server/pages/api/getProductCategory.js
+++ Server/pages/api/getProductCategory.js
@@ -1,57 +1,53 @@
 import excuteQuery from './db';
 
 
-export async function getProductCategory(category, receipt, brand, season) {
-    try {
+async function getProductCategory(brand, season) {
 
-	var q = "SELECT * FROM product_category WHERE brand_id=? AND season_type=? ORDER BY category_name";
 
-        if(category == 1 && receipt == 1){
-    	    //고객용, 수선 -> 수선처
-        }
-        else{
-	
-	    //나머지 -> 본사
-	    brand=0;
-	    season=0;
-        }
+
+        // if(category == 1 && receipt == 1){
+    	//     //고객용, 수선 -> 수선처
+        // }
+        // else{
+        //     //나머지 -> 본사
+        //     brand=0;
+        //     season=0;
+        // }
 	
 
-        const result = await excuteQuery({
-            query: q,
+        return excuteQuery({
+            query: "SELECT pcategory_id, category_name FROM product_category WHERE brand_id=? AND season_type=? ORDER BY category_name",
             values: [brand, season]
         });
 
-	//console.log(result);
-        return result;
-
-    } catch (error) {
-        console.log(error);
-    }
 }
 
-export default (req, res) => {
+const controller =  async (req, res) => {
   if (req.method === "POST") {
     console.log("req");
     console.log(req.body);
-    var category = req.body.category; 	//1:고객용 2:매장용 3:선처리
-    var receipt = req.body.receipt; 	//1: 수선 2: 교환 3: 환불 4: 심의
-    var brand = req.body.brand; 	//brand id
-    var season = req.body.season; 	//0: 당시즌 1: 과시즌 
+    // const category = req.body.category; 	//1:고객용 2:매장용 3:선처리
+    // const receipt = req.body.receipt; 	//1: 수선 2: 교환 3: 환불 4: 심의
+    const brand = req.body.brand; 	//brand id
+    const season = req.body.season; 	//0: 당시즌 1: 과시즌 
 
-    getProductCategory(category, receipt, brand, season).then((body) => {
+    try{
+        const results = await getProductCategory(brand, season);
+        if (results.error) throw new Error(results.error);
 
-        if (body.length){
-                console.log("Product Category Info");
-                res.statusCode = 200;
-                res.json({body});
-        }
-        else{
-                console.log("Product Category Fail");
-                res.status(400).send();
-        }
-
-    });
+        if (results.length){
+            console.log("Product Category Info");
+            res.status(200).json({body: results});
+    }
+    else{
+            console.log("No Product Category");
+            res.status(204).send();
+    }
+    }catch(err){
+        console.log(err.message);
+        res.status(500).json(err);
+    }
   }
 };
 
+export default controller;
---
+++ Server/pages/api/getRepairList.js
@@ -0,0 +1,50 @@
+import excuteQuery from './db';
+
+
+async function getHeadquarters() {
+    return excuteQuery({
+        query: "SELECT store_id AS receiver_id, name AS receiver_name FROM store WHERE store_type=0 ORDER BY name",
+        values: []
+    });
+}
+
+async function getRepairPlaces(pcategoryId) {
+    return excuteQuery({
+        query: "SELECT receiver_id, receiver_name FROM product_category WHERE pcategory_id=?",
+        values: [pcategoryId]
+    });
+
+}
+
+const controller =  async (req, res) => {
+  if (req.method === "POST") {
+    console.log("req");
+    console.log(req.body);
+    const category = req.body.category; 	//1:고객용 2:매장용 3:선처리
+    const receipt = req.body.receipt; 	//1: 수선 2: 교환 3: 환불 4: 심의
+    const pcategoryId = req.body.pcategory_id; 	//pcategory id
+
+    try{
+        const headquarters = await getHeadquarters();
+        if (headquarters.error) throw new Error(headquarters.error);
+
+        let repairPlaces = [];
+        if(category == 1 && receipt == 1) {
+            repairPlaces = await getRepairPlaces(pcategoryId);
+            if (repairPlaces.error) throw new Error(repairPlaces.error);
+        }
+
+        const list = [...repairPlaces, ...headquarters];
+        res.status(200).json({
+            selected: list[0].receiver_id,
+            list,
+        });
+
+    }catch(err){
+        console.log(err.message);
+        res.status(500).json(err);
+    }
+  }
+};
+
+export default controller;
