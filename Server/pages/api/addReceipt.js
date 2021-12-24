63a96e7fbcb82cb0db6120a9c95d188475632438
ahrimy <dlsdjdkfla@gmail.com>
2021-12-24 15:40:52 +0900

    receipt 생성 api 수정

--- Server/pages/api/addReceipt.js
+++ Server/pages/api/addReceipt.js
@@ -1,47 +1,59 @@
-import excuteQuery from './db';
+import excuteQuery from "./db";
 
-
-export async function addReceipt(store, staff, customer, category, pid, pcode, substitute) {
-    try {
-        const result = await excuteQuery({
-            query: "INSERT INTO `receipt`(`store_id`, `staff_id`, `customer_id`,`category`, `product_id`, `product_code`, `substitute`) VALUES (?,?,?,?,?,?,?)",
-            values: [store, staff, customer, category, pid, pcode, substitute]
-        });
-
-	//console.log("id"+ result['insertId']);
-        return result;
-
-    } catch (error) {
-        console.log(error);
-    }
+async function addReceipt(
+  store,
+  staff,
+  customer,
+  category,
+  pid,
+  pcode,
+  substitute,
+  mfrid
+) {
+  return excuteQuery({
+    query:
+      "INSERT INTO `receipt`(`store_id`, `staff_id`, `customer_id`,`category`, `product_id`, `product_code`, `substitute`, `mfr_id`) VALUES (?,?,?,?,?,?,?,?)",
+    values: [store, staff, customer, category, pid, pcode, substitute, mfrid],
+  });
 }
 
-
-export default (req, res) => {
+const controller = async (req, res) => {
   if (req.method === "POST") {
     console.log("req");
     console.log(req.body);
-    var store = req.body.store; 
-    var staff = req.body.staff; 
-    var customer = req.body.customer; 
-    var category = req.body.category; 
-    var pid = req.body.pid; 
-    var pcode = req.body.pcode; 
-    var substitute = req.body.substitute;
+    const store = req.body.store;
+    const staff = req.body.staff;
+    const customer = req.body.customer;
+    const category = req.body.category;
+    const pid = req.body.pid;
+    const pcode = req.body.pcode;
+    const substitute = req.body.substitute || 0;
+    const mfrid = req.body.mfrid;
 
-    addReceipt(store, staff, customer, category, pid, pcode, substitute).then((body) => {
-
-	if(body['affectedRows']){
-                console.log("add Receipt (step 2)");
-                res.statusCode = 200;
-                res.json({receipt_id: body['insertId']});
-        }
-        else{
-                console.log("add Receipt (step2) failed");
-                res.status(400).send();
-        }
-
-    });
+    try {
+      const results = await addReceipt(
+        store,
+        staff,
+        customer,
+        category,
+        pid,
+        pcode,
+        substitute,
+        mfrid
+      );
+      if (results.error) throw new Error(results.error);
+      if (results["affectedRows"]) {
+        console.log("add Receipt (step 2)");
+        res.status(200).json({ receipt_id: results["insertId"] });
+      } else {
+        console.log("add Receipt (step2) failed");
+        res.status(400).send();
+      }
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).json(err);
+    }
   }
 };
 
+export default controller;
