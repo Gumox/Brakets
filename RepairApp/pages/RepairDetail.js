314ae4302c41745b6179d80acca846804546a031
Jwon <check4545@daum.net>
2022-01-13 13:39:35 +0900

    Update RepairDetail.js

--- RepairApp/pages/RepairDetail.js
+++ RepairApp/pages/RepairDetail.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, Image, Alert } from 'react-native';
 import dayjs from 'dayjs';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
@@ -9,6 +9,7 @@ import styled from 'styled-components/native';
 import ContainView from '../components/ContainView';
 import SmallButton from '../components/SmallButton';
 import Bottom from '../components/Bottom';
+import { TextInput } from 'react-native-gesture-handler';
 
 
 function RepairDetail({ navigation, route }) {
@@ -27,7 +28,15 @@ function RepairDetail({ navigation, route }) {
     const [shippingPlace, setShippingPlace] = useState('');
 
     const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-    const [btnDisabled, setBtnDiabled] = useState(true)
+    const [btnDisabled, setBtnDiabled] = useState(true);
+
+    useEffect(() => {
+
+        console.log("shipping date is: " + shippingDate);
+
+      }, [shippingDate]);
+
+    
 
     const showDatePicker = () => {
         setDatePickerVisibility(true);
@@ -38,9 +47,9 @@ function RepairDetail({ navigation, route }) {
     };
 
     const handleConfirm = (date) => {
-        console.warn("A date has been picked: ", date);
-        setShippingDate(date);
-        console.log(shippingDate);
+        console.log("A date has been picked: ", date.toLocaleDateString('ko-kr'));
+        setShippingDate(date.toLocaleDateString('ko-kr'));
+        console.log("shipping date is" + shippingDate);
         hideDatePicker();
     };
 
@@ -58,7 +67,6 @@ function RepairDetail({ navigation, route }) {
                         value={cardId}
                         onChangeText={text => setCardID(text)}
                     />
-
 
                     <Label>브랜드 번호</Label>
                     <Input
@@ -115,7 +123,7 @@ function RepairDetail({ navigation, route }) {
 
                     <Label>매장 접수 내용</Label>
                     <BiggerInput multiline={true}>
-                        
+
                     </BiggerInput>
                 </OverallView>
 
@@ -198,8 +206,14 @@ function RepairDetail({ navigation, route }) {
 
                 <OverallView>
                     <Label>수선처 발송일</Label>
-                    <PickerView
-                        onPress={() => showDatePicker()}>
+                    <DatePickerButton onPress={(showDatePicker)}>
+                    
+                        <DateInput
+                            pointerEvents="none"
+                            editable={false}
+                            value={shippingDate}
+                        />
+
                         <DateTimePickerModal
                             isVisible={isDatePickerVisible}
                             mode="date"
@@ -207,19 +221,19 @@ function RepairDetail({ navigation, route }) {
                             onConfirm={handleConfirm}
                             onCancel={hideDatePicker}
                         />
+                    </DatePickerButton>
 
-                    </PickerView>
                     <Label>발송 방법</Label>
                     <PickerView>
                         <RNPickerSelect
-                            placeholder = {{label : '[필수] 옵션을 선택하세요',value: null}}
+                            placeholder={{ label: '[필수] 옵션을 선택하세요', value: null }}
                             onValueChange={(value) => setShippingMethod(value)}
-                                items={[
-                                    { label: '1. ', value: '1.'}
-                                ]}
-                            /> 
+                            items={[
+                                { label: '1. ', value: '1.' }
+                            ]}
+                        />
                     </PickerView>
-                    
+
                     <Label>발송 비용</Label>
                     <Input
                         editable={true}
@@ -231,13 +245,13 @@ function RepairDetail({ navigation, route }) {
                     <Label>받는 곳</Label>
                     <PickerView>
                         <RNPickerSelect
-                            placeholder = {{label : '[필수] 옵션을 선택하세요',value: null}}
-                            style = { {border :'solid', marginBottom : '50', borderWidth : '3', borderColor : 'black'} }
+                            placeholder={{ label: '[필수] 옵션을 선택하세요', value: null }}
+                            style={{ border: 'solid', marginBottom: '50', borderWidth: '3', borderColor: 'black' }}
                             onValueChange={(value) => setShippingPlace(value)}
-                                items={[
-                                    { label: '1. ', value: '1. '}
-                                ]}
-                            /> 
+                            items={[
+                                { label: '1. ', value: '1. ' }
+                            ]}
+                        />
                     </PickerView>
 
                 </OverallView>
@@ -246,21 +260,23 @@ function RepairDetail({ navigation, route }) {
                 <BottomView>
                     <SmallButton onPress={() =>
                         navigation.navigate('RepairStepOne')
-                        
+
                     }>
                         <Text>
                             취소
                         </Text>
                     </SmallButton>
 
-                    <SmallButton 
-                        onPress={() => {(btnDisabled == true) ? (
-                            Alert.alert('수선처 발송일을 입력해주세요')
-                        ) : ( 
+                    <SmallButton
+                        onPress={() => {
+                            (btnDisabled == true) ? (
+                                Alert.alert('수선처 발송일을 입력해주세요')
+                            ) : (
                             console.log('저장 기능 구현')
-                        )}
-                    
-                    }
+                        )
+                        }
+
+                        }
                     >
                         <Text>
                             저장
@@ -277,6 +293,17 @@ function RepairDetail({ navigation, route }) {
 
 
 export default RepairDetail;
+
+const DatePickerButton = styled.TouchableOpacity``;
+
+const DateInput = styled.TextInput`
+    width: 100%;
+    padding: 8px;
+    font-size: 20px;
+    border : 1.5px;
+    border-radius:10px
+    height: 35px;
+`;
 
 const PickerView = styled.TouchableOpacity`
     width: 100%;
