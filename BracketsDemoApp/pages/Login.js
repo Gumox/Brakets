71da29820d3560dcfca6e708ba2aeeb607dea885
Jwon <check4545@daum.net>
2022-01-21 16:57:48 +0900

    fix Login

--- BracketsDemoApp/pages/Login.js
+++ BracketsDemoApp/pages/Login.js
@@ -19,13 +19,48 @@ function Login({ navigation }): React.ReactElement {
 
   const [userId, setUserID] = useState('');
   const [userEmail, setUserEmail] = useState('');
+  const [isOn, setIsOn] = useState(false);
 
   const isFirstRun = useRef(true);
+
+  function SaveInfo(_userName, _userEmail, _data){
+
+      AsyncStorage.setItem(
+        'userInfo',
+        JSON.stringify({
+          'userName': _userName,
+          'userEmail': _userEmail,
+          "data": _data
+        }), () => {
+          console.log('user id and user email\'s saved: ' + _userName, _userEmail);
+          store.dispatch({ type: 'storeStaffId', storeStaffId: _userEmail });
+          store.dispatch({ type: 'storeName', storeName: _userName })
+          store.dispatch({ type: 'USER_INFO', userInfo: _data })
+          navigation.replace('ReceiptDivision');
+        });
+  }
+
+  function LoadInfo(){
+
+    AsyncStorage.getItem('userInfo', (err, result) => {
+      if (result !== null) {
+        const UserInfo = JSON.parse(result);
+        console.log(UserInfo.data[0])
+        store.dispatch({ type: 'USER_INFO', userInfo: UserInfo.data })
+        store.dispatch({ type: 'storeStaffId', storeStaffId: UserInfo.userEmail });
+        store.dispatch({ type: 'storeName', storeName: UserInfo.userName })
+        // console.log(store.getState().userInfo[0].staff_id)
+        navigation.replace('ReceiptDivision');
+
+      }
+    })
+  }
 
   useEffect(() => {
 
     if (isFirstRun.current) {
       isFirstRun.current = false;
+      LoadInfo();
       return;
     }
 
@@ -44,8 +79,7 @@ function Login({ navigation }): React.ReactElement {
     axios(option)
       .then(
         response => (response.status == '200') && (response.data.data[0].name) ? (
-          savedInfo(response.data.data),
-          saveId(response.data.data[0].name, userEmail)
+          SaveInfo(response.data.data[0].name, userEmail, response.data.data)
         ) : (
           console.log("response" + response)
         )
@@ -60,54 +94,8 @@ function Login({ navigation }): React.ReactElement {
           ]
         )
       })
-  });
+  }, [isOn]);
 
-  function saveId(_userName, _userEmail) {
-
-    AsyncStorage.setItem(
-      'userInfo',
-      JSON.stringify({
-        'userEmail': _userEmail,
-        'userName': _userName
-      }), () => {
-        console.log('user id and user email\'s saved: ' + _userName, _userEmail);
-        store.dispatch({ type: 'storeStaffId', storeStaffId: _userEmail });
-        store.dispatch({ type: 'storeName', storeName: _userName })
-        navigation.replace('ReceiptDivision');
-      });
-  }
-
-
-  function savedInfo(data) {
-    AsyncStorage.setItem(
-      'info',
-      JSON.stringify({
-        "data": data
-      }), () => {
-        store.dispatch({ type: 'USER_INFO', userInfo: data })
-        console.log(data)
-        console.log(store.getState().userInfo)
-      });
-  }
-  const savedResultInfo = AsyncStorage.getItem('info', (err, result) => {
-    if (result !== null) {
-      const UserInfo = JSON.parse(result);
-      console.log(UserInfo.data[0])
-      store.dispatch({ type: 'USER_INFO', userInfo: UserInfo.data })
-      console.log(store.getState().userInfo[0].staff_id)
-    }
-  })
-
-  const savedResultId = AsyncStorage.getItem('userInfo', (err, result) => {
-    //user_id에 담긴 아이디 불러오기
-    if (result !== null) {
-      const UserInfo = JSON.parse(result);
-      //console.log(UserInfo)
-      store.dispatch({ type: 'storeStaffId', storeStaffId: UserInfo.userEmail });
-      store.dispatch({ type: 'storeName', storeName: UserInfo.userName })
-      navigation.replace('ReceiptDivision');
-    }
-  });
 
 
   const signInWithKakao = async (): Promise<void> => {
@@ -132,6 +120,7 @@ function Login({ navigation }): React.ReactElement {
 
     setUserID(profile.id);
     setUserEmail(profile.email);
+    setIsOn(!isOn);
 
     console.log("(get profile)user id: " + userId);
     console.log("(get profile)user email: " + userEmail);
