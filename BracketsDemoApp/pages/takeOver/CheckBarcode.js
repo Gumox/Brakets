e66e62d7959b7224f701028bb3818d8beb708a02
Jwon <check4545@daum.net>
2022-01-21 11:07:21 +0900

    Update CheckBarcode.js

--- BracketsDemoApp/pages/takeOver/CheckBarcode.js
+++ BracketsDemoApp/pages/takeOver/CheckBarcode.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 import Container from '../../components/Container';
 import Button from '../../components/Button';
 import styled from 'styled-components/native';
 import CenterText from '../../components/CenterText';
-import _ from 'lodash';
 import Contents from '../../components/Contents';
-import TopInfo from '../../components/TopInfo';
 import Bottom from '../../components/Bottom';
-import StateBarSolid from '../../components/StateBarSolid';
-import store from '../../store/store';
-
 
 const RegistText= styled.Text`
     font-weight: bold;
@@ -27,11 +21,19 @@ const GrayText = styled.Text`
     color:#858585;
 `;
 const TopStateView = styled.View`
-    
     flex-direction: row;
     padding:24px;
     justify-content: center;
 `;
+const AlternativeCodeText = styled.Text`
+    
+`;
+const AlternativeCodeBtn = styled.TouchableOpacity`
+    justify-content: center;
+    align-items: center;
+`;
+
+
 // 구조 분해 할당, Destructuring Assignment
 export default function CheckBarcode( { navigation } ) {
     //console.log(store.getState().recDate);
@@ -39,17 +41,22 @@ export default function CheckBarcode( { navigation } ) {
     return (
         <Container>
             <CenterText>
-                
-                
                 <RegistText>인수 확인</RegistText>
-            
-                
             </CenterText>
-            <Contents>
-                
+            
+            <Contents>  
                 <BlueText>서비스 카드 바코드</BlueText>
                 <GrayText>를 스캔하세요</GrayText>
             </Contents>
+            
+            <AlternativeCodeBtn
+                onPress = {() => navigation.navigate('InputAlternativeNumber', {key:'TakeOverPage'})}
+            >
+                <AlternativeCodeText>
+                    대체 코드 입력
+                </AlternativeCodeText>
+            </AlternativeCodeBtn>
+
             <Button onPress={ ()=> navigation.navigate( 'ScanScreen',{key:'TakeOverPage'} ) }>
                 코드 스캔
             </Button>
