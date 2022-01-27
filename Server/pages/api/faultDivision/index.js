74989b8711e4ce757699c15154aeb043c125f2c9
Gumox <fodhs1000@naver.com>
2022-01-27 16:10:52 +0900

    make api fault_division

---
+++ Server/pages/api/faultDivision/index.js
@@ -0,0 +1,43 @@
+import excuteQuery from "../db"
+
+async function getFaultDivision(id) {
+    const result = await excuteQuery({
+        query: `SELECT  *
+                FROM fault_division 
+                WHERE hq_id = ?`,
+        values:[id],
+      });
+    
+      return result;
+}
+
+const controller = async (req, res) => {
+    if (req.method === "GET") {
+        console.log("req.headers.referer");
+        console.log(req.headers.referer);
+        console.log("req.query");
+        console.log(req.query);
+        
+        const {
+            hq_id
+        } = req.query;
+        
+
+    try {
+      const result = await getFaultDivision(hq_id);
+      if (result.error) throw new Error(result.error);
+      if (result.length) {
+        console.log("result");
+        res.status(200).json({ body: result });
+      } else {
+        console.log("No result");
+        res.status(204).json({ message: "No result" });
+      }
+    } catch (err) {
+      console.log(err.message);
+      res.status(400).json({ err: err.message });
+    }
+  }
+};
+
+export default controller;
