ac9e6efef61b3748c69e117186ca9c36229bf9cd
Jwon <check4545@daum.net>
2022-01-30 22:10:31 +0900

    fix

--- BracketsDemoApp/pages/takeOver/TakeOverPage.js
+++ BracketsDemoApp/pages/takeOver/TakeOverPage.js
@@ -48,7 +48,7 @@ const Input = styled.TextInput`
     padding: 8px;
     font-size: 20px;
     background-color:#d6d6d6;
-    border-radius:10px
+    border-radius:10px;
 `;
 const InputText = styled.Text`
     color: #000000;
@@ -56,7 +56,7 @@ const InputText = styled.Text`
     padding: 8px;
     font-size: 20px;
     background-color:#d6d6d6;
-    border-radius:10px
+    border-radius:10px;
 `;
 const Half = styled.View`
     width : 100%;
