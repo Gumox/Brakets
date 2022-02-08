1d867c2a5c64b3889be48253c398454b0b738e28
Gumox <fodhs1000@naver.com>
2022-02-08 19:37:04 +0900

    수선처 웹 반송

---
+++ Server/pages/api/RepairShop/unregistered/getTargetInfo.js/index.js
@@ -0,0 +1,43 @@
+import excuteQuery from "../../../db";
+
+async function getReceipt(values) {
+    const result = await excuteQuery({
+        query: `SELECT  receipt.receipt_code,
+                        store.store_id AS store_id,
+                        store.name AS store_name,
+                        brand.brand_name,
+                        brand.brand_id,
+                        receiver.name AS receiver_name,
+                        receipt.receiver AS receiver_id
+                FROM receipt 
+                LEFT JOIN store ON store.store_id = receipt.store_id
+                LEFT JOIN product ON product.product_id = receipt.product_id
+                LEFT JOIN brand ON brand.brand_id = product.brand_id
+                LEFT JOIN store AS receiver ON receipt.receiver = receiver.store_id 
+                WHERE receipt.receipt_code = ?  `,
+        values,
+      });
+    
+      return result;
+}
+
+
+const getTargetInfo = async (req, res) => {
+  if (req.method === "GET") {
+    console.log("/api/RepairShop/unregistered/getTargetInfo");
+    const {
+        code,
+    } = req.query;
+    try {
+      const result = await getReceipt(code);
+      if (result.error) throw new Error(result.error);
+
+      res.status(200).json({ data: result });
+    } catch (err) {
+      console.log(err.message);
+      res.status(400).json({ err: err.message });
+    }
+  }
+};
+
+export default getTargetInfo;
--- Server/pages/return_unregistered.js
+++ Server/pages/return_unregistered.js
@@ -1,21 +1,77 @@
-import React,{useEffect, useState} from 'react';
+import React,{useEffect, useState,useCallback} from 'react';
 import RepairHeader from '../components/RepairHeader'
 import styled from 'styled-components';
 import COLOR from '../constants/color';
 import axios from 'axios';
+import formatDate from '../functions/formatDate';
 import store from '../store/store';
 
 export default function Return_unregistered() {
     
     const [selectedCompany,setSelectedCompany] = useState(null)
     const [companyList,setCompanyList] = useState(store.getState().company)
+    const [code,setCode] = useState()
+    const [brand,setBrand] = useState(null)
+    const [brandList,setBrandList] = useState([])
+    const [repairShopList,setRepairShopList] = useState([])
     const shop_id = store.getState().shop
+    const [returnList,setReturnList] = useState([])
     
     const getInfo = async(shop_id)=>{
 
     }
-    useEffect(()=>{
+    const getBrandList = async()=>{
+        const[datas] =await Promise.all([
+            axios.get(`${process.env.API_URL}/brand/AllBrandList`)
+            .then(({ data }) => data.data),
+          ])
+          return datas;
+    }
+    const getRepairShopList = async()=>{
+        const[datas] =await Promise.all([
+            axios.get(`${process.env.API_URL}/store/getAllRepairShop`)
+            .then(({ data }) => data.data),
+          ])
+          return datas;
+    }
+    const getTargetInfo = async(code)=>{
+        const[datas] =await Promise.all([
+            axios.get(`${process.env.API_URL}/RepairShop/unregistered/getTargetInfo?code=${code}`)
+            .then(({ data }) => data.data),
+          ])
+          return datas;
+    }
+    const handleKeyPress = useCallback(async(e,code) => {
+          if (e.key !== "Enter"){return;}
+          else{
+            let input = await getTargetInfo(code);
+            
+            if(input.length){
+                input[0].return_date = new Date();
+
+                returnList.map((item)=>{
+                    if(item.receipt_code !== input[0].receipt_code){
+                        returnList.push(input[0])
+                    }
+                })
+                if(returnList[0]===undefined){returnList.push(input[0])}
+                console.log(returnList[0])
+            }else{
+                alert("잘못된 서비스카드 번호 입니다")
+            }
+          }
+        },[]
+      );
+    useEffect( async()=>{
         setCompanyList(JSON.parse(localStorage.getItem('COMPANY')))
+        let list =await getBrandList();
+        list.unshift({brand_id: "",brand_name: "전체"})
+
+        let list2 =await getRepairShopList();
+        list2.unshift({store_id: "",name: "전체"})
+        
+        setBrandList(list);
+        setRepairShopList(list2)
     },[])
     return(
         <div style={{minWidth:1150, overflowY:"scroll"}}>
@@ -34,7 +90,7 @@ export default function Return_unregistered() {
                     </select>
                     <button 
                         style={{marginLeft:10,width:40,height:22,fontSize:12,backgroundColor : "#4f4f4f", color: COLOR.WHITE}}
-                        onClick={()=>{getOptions()}}>
+                        onClick={()=>{}}>
                             확인
                     </button>  
                     </div>
@@ -51,16 +107,20 @@ export default function Return_unregistered() {
                     <CenterView>
                     수선처 : 
                     <select name="soosun"  style={{marginLeft:10,marginRight: 10}} >
-                        <option value="soo">수선처1</option>
-                        <option value="soos">수선처2</option>
-                        <option value="soosu">수선처3</option>
+                        {   
+                            repairShopList.map((item,index)=>(
+                                <option key={index} value={item.store_id}>{item.name}</option>
+                            ))
+                        }
                     </select>
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     브랜드 : 
                     <select name="brand"  style={{marginLeft:10,marginRight: 10}} >
-                        <option value="br">브랜드1</option>
-                        <option value="bra">브랜드2</option>
-                        <option value="bran">브랜드3</option>
+                        {   
+                            brandList.map((item,index)=>(
+                                <option key={index} value={item.brand_id}>{item.brand_name}</option>
+                            ))
+                        }
                     </select>
                     
                     
@@ -69,7 +129,7 @@ export default function Return_unregistered() {
                     <input type="date"  style={{marginLeft:20,height:22}}></input>
                     <button 
                         style={{marginLeft:10,width:40,height:22,fontSize:12,backgroundColor : "#4f4f4f", color: COLOR.WHITE}}
-                        onClick={()=>{getOptions()}}
+                        onClick={()=>{}}
                         >조회</button>  
                     </CenterView>
                 </Container> 
@@ -79,11 +139,11 @@ export default function Return_unregistered() {
                 
                    
                     <CenterView>       
-                        <div style={{fontWeight:"bold"}}>서비스 카드 번호 : </div>
-                        <input style={{marginLeft:15,height:22}}></input> 
+                        <div style={{fontWeight:"bold"}} >서비스 카드 번호 : </div>
+                        <input style={{marginLeft:15,height:22}} onChange={(e)=>{setCode(e.target.value)}} onKeyPress={(e)=>{handleKeyPress(e,code)}}></input> 
                         <button 
                             style={{marginLeft:10,width:40,height:22,fontSize:12,backgroundColor : "#4f4f4f", color: COLOR.WHITE}}
-                            onClick={()=>{getOptions()}}
+                            onClick={()=>{}}
                         >추가</button> 
                         
                     </CenterView>
@@ -100,7 +160,7 @@ export default function Return_unregistered() {
             
             <Line/>
             <ItemTable >
-                <div style={{marginTop:12,overflowY:"scroll",maxHeight: 400,maxWidth:"100%"}}>
+                
                 <LaView><Container>
                     <ItemView>#</ItemView>
                     <ItemView>수선처</ItemView>
@@ -110,7 +170,19 @@ export default function Return_unregistered() {
                     <ItemView>브랜드</ItemView>
                 </Container></LaView>
                 <Line2/>
-
+                <div style={{marginTop:12,overflowY:"scroll",maxHeight: 400,maxWidth:"100%",minHeight:200}}>
+                    {
+                        returnList.map((item,index)=>(
+                            <LaView key={index}>
+                                <ItemView>{index+1}</ItemView>
+                                <ItemView>{item.receiver_name}</ItemView>
+                                <ItemView>{formatDate(item.return_date)}</ItemView>
+                                <ItemView>{item.receipt_code}</ItemView>
+                                <ItemView>{item.store_name}</ItemView>
+                                <ItemView>{item.brand_name}</ItemView>
+                            </LaView>
+                        ))
+                    }
                 </div>
               
               
