c30a533d1a2127a1f0161c62f5467b1f4f298c34
Gumox <fodhs1000@naver.com>
2022-02-14 18:48:44 +0900

    Update RepairReception.js

--- Server/pages/RepairReception.js
+++ Server/pages/RepairReception.js
@@ -4,7 +4,6 @@ import styled from "styled-components";
 import COLOR from "../constants/color";
 import axios from "axios";
 import _ from "lodash";
-import RepairReceiptModal from "../components/RepairReceiptModal";
 import store from "../store/store";
 import headers from "../constants/repairReceptionTableHeader";
 import checkDisable from "../functions/checkDisable";
@@ -48,7 +47,9 @@ function RepairReception({options,user}) {
   listData.forEach((el,index) => {
       let items=(
         <div key={index}>
-          <RepairReceiptModal item={el} info ={options.info[0]} images ={options.images}></RepairReceiptModal>
+          {
+          //<RepairReceiptModal item={el} info ={options.info[0]} images ={options.images}></RepairReceiptModal>
+          }
         </div>
     )
     lists[index] = items;
