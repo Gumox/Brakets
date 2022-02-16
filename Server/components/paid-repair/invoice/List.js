bc0262a8ca75dfa06da200a942f80aacd8597167
Jwon <check4545@daum.net>
2022-02-16 15:18:40 +0900

    implement paid-repair table

---
+++ Server/components/paid-repair/invoice/List.js
@@ -0,0 +1,200 @@
+import React from "react";
+import styled from "styled-components";
+import moment from "moment";
+import { useTable, useBlockLayout, useResizeColumns } from 'react-table';
+
+import COLOR from "../../../constants/color";
+
+function Table({ columns, data, searchList, getTargetData }) {
+
+  const defaultColumn = React.useMemo(
+      () => ({
+          minWidth: 100,
+          width: 100,
+          maxWidth: 150,
+      }),
+      []
+  )
+
+  const {
+      getTableProps,
+      getTableBodyProps,
+      headerGroups,
+      rows,
+      prepareRow,
+      state,
+      resetResizing,
+  } = useTable(
+      {
+          columns,
+          data,
+          defaultColumn,
+      },
+      useBlockLayout,
+      useResizeColumns
+  )
+
+  return (
+      <>
+          <div>
+              <div {...getTableProps()} className="table">
+                  <div>
+                      {headerGroups.map((headerGroup,i) => (
+                          <div key={i}{...headerGroup.getHeaderGroupProps()} className="tr">
+                              {headerGroup.headers.map((column,j) => (
+                                  <div key={j} {...column.getHeaderProps()} className="th">
+                                      {column.render('Header')}
+                                      {/* Use column.getResizerProps to hook up the events correctly */}
+                                      <div
+                                          {...column.getResizerProps()}
+                                          className={`resizer ${column.isResizing ? 'isResizing' : ''
+                                              }`}
+                                      />
+                                  </div>
+                              ))}
+                          </div>
+                      ))}
+                  </div>
+
+                  <div {...getTableBodyProps()}>
+                      {rows.map((row, i) => {
+                          prepareRow(row)
+                          return (
+                              <div key={i} {...row.getRowProps(
+                                  {onClick: () => (getTargetData(row.original["서비스카드 번호"]))}
+                              )} className="tr">
+                                  {row.cells.map((cell,j) => {
+                                      return (
+                                          <div key={j} {...cell.getCellProps()} className="td">
+                                              {cell.render('Cell')}
+                                          </div>
+                                      )
+                                  })}
+                              </div>
+                          )
+                      })}
+                  </div>
+              </div>
+          </div>
+      </>
+  )
+}
+
+const List = ({ data, handleDataClick = () => {} }) => {
+
+  const columns = React.useMemo(() => [
+    {Header: 'No', accessor: 'No'},
+    {Header: '매장코드',   accessor: '매장코드'},
+    {Header: '매장명', accessor: '매장명'},
+    {Header: '출고일', accessor: '출고일'},
+    {Header: '상태', accessor: '상태'},
+    {Header: '시즌', accessor: '시즌'},
+    {Header: 'PartCode', accessor: 'PartCode'},
+    {Header: '컬러', accessor: '컬러'},
+    {Header: '사이즈', accessor: '사이즈'},
+    {Header: '수량', accessor: '수량'},
+    {Header: '금액', accessor: '금액'},
+    {Header: '출고율', accessor: '출고율'},
+    {Header: '출고금액', accessor: '출고금액'},  
+    {Header: '부과세', accessor: '부과세'},
+    {Header: '출고금액적용일', accessor: '출고금액적용일'},
+    {Header: 'SORDER NO.', accessor: 'SORDER NO.'},
+    {Header: 'INVOICE NO.', accessor: 'INVOICE NO.'},
+    {Header: '최초생성자', accessor: '최초생성자'},
+    {Header: '최초생성일', accessor: '최초생성일'},
+    {Header: '최초수정자', accessor: '최초수정자'},
+    {Header: '최초수정일', accessor: '최초수정일'},
+  ],[])
+
+    const value = [];
+
+  return (
+    <Wrapper>
+      <Styles>
+        <Table columns={columns} data={value}/>
+      </Styles>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  height: calc(100% - 120px);
+  width: 100%;
+  overflow: scroll;
+  border-bottom: 2px solid;
+`;
+
+const Styles = styled.div`
+  padding: 1rem;
+
+  .table {
+    display: inline-block;
+    border-spacing: 0;
+    border: 1px solid black;
+
+    .tr {
+      :last-child {
+        .td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    .th,
+    .td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      ${'' /* In this example we use an absolutely position resizer,
+       so this is required. */}
+      position: relative;
+
+      :last-child {
+        border-right: 0;
+      }
+
+      .resizer {
+        display: inline-block;
+        /* background: black; */
+        width: 2px;
+        height: 100%;
+        position: absolute;
+        right: 0;
+        top: 0;
+        transform: translateX(50%);
+        z-index: 1;
+        ${'' /* prevents from scrolling while dragging on touch devices */}
+        touch-action:none;
+      }
+    }
+  }
+`
+
+const TableHeader = styled.thead`
+  border: 2px solid ${COLOR.BLACK};
+`;
+
+const TableHeaderCell = styled.th`
+  width: ${({ width = "100px" }) => width};
+  min-width: ${({ width = "100px" }) => width};
+  border: 2px solid ${COLOR.BLACK};
+`;
+
+const TableRow = styled.tr`
+  cursor: pointer;
+`;
+
+const TableData = styled.td`
+  max-width: ${({ width = "100px" }) => width};
+  width: ${({ width = "100px" }) => width};
+  min-width: ${({ width = "100px" }) => width};
+  text-align: ${({ textAlign = "center" }) => textAlign};
+  border: 1px solid ${COLOR.GRAY};
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+`;
+
+export default List;
