69c12920d75f656be26777e5eee41a0b23cd38f4
Jwon <check4545@daum.net>
2022-02-16 15:19:25 +0900

    implement paid-repair

--- Server/components/paid-repair/list/index.js
+++ Server/components/paid-repair/list/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import moment from "moment";
+import { useTable, useBlockLayout, useResizeColumns } from 'react-table';
 
 import COLOR from "../../../constants/color";
 import { RECEIPT, CUSTOMER, STORE, PRODUCT } from "../../../constants/field";
@@ -8,106 +9,207 @@ import {
   STORE_TYPE,
   RECEIPT_CATEGORY_TYPE,
   RECEIPT_TYPE,
+  STORE_CATEGORY
 } from "../../../constants/type";
 import Checkbox from "../../Checkbox";
 
+function Table({ columns, data, searchList, getTargetData }) {
+
+  const defaultColumn = React.useMemo(
+      () => ({
+          minWidth: 100,
+          width: 100,
+          maxWidth: 150,
+      }),
+      []
+  )
+
+  const {
+      getTableProps,
+      getTableBodyProps,
+      headerGroups,
+      rows,
+      prepareRow,
+      state,
+      resetResizing,
+  } = useTable(
+      {
+          columns,
+          data,
+          defaultColumn,
+      },
+      useBlockLayout,
+      useResizeColumns
+  )
+
+  return (
+      <>
+          <div>
+              <div {...getTableProps()} className="table">
+                  <div>
+                      {headerGroups.map((headerGroup,i) => (
+                          <div key={i}{...headerGroup.getHeaderGroupProps()} className="tr">
+                              {headerGroup.headers.map((column,j) => (
+                                  <div key={j} {...column.getHeaderProps()} className="th">
+                                      {column.render('Header')}
+                                      {/* Use column.getResizerProps to hook up the events correctly */}
+                                      <div
+                                          {...column.getResizerProps()}
+                                          className={`resizer ${column.isResizing ? 'isResizing' : ''
+                                              }`}
+                                      />
+                                  </div>
+                              ))}
+                          </div>
+                      ))}
+                  </div>
+
+                  <div {...getTableBodyProps()}>
+                      {rows.map((row, i) => {
+                          prepareRow(row)
+                          return (
+                              <div key={i} {...row.getRowProps(
+                                  // {onClick: () => (getTargetData(row.original["서비스카드 번호"]))}
+                              )} className="tr">
+                                  {row.cells.map((cell,j) => {
+                                      return (
+                                          <div key={j} {...cell.getCellProps()} className="td">
+                                              {cell.render('Cell')}
+                                          </div>
+                                      )
+                                  })}
+                              </div>
+                          )
+                      })}
+                  </div>
+              </div>
+          </div>
+      </>
+  )
+}
+
 const ReturnList = ({ data, handleDataClick = () => {} }) => {
+
+  const columns = React.useMemo(() => [
+    
+    // 단순 조회 기능 -> Check Box 필요 없을 듯?
+    // {Header: '선택',  accessor: '선택',},
+
+    {Header: '서비스카드 번호', accessor: '서비스카드 번호',},
+    {Header: '매장코드',   accessor: '매장코드',},
+    {Header: '매장명', accessor: '매장명',},
+    {Header: '매장구분', accessor: '매장구분',},
+    {Header: '매장연락처', accessor: '매장연락처',},
+    {Header: '등록일', accessor: '등록일',},
+    {Header: '고객ID', accessor: '고객ID',},
+    {Header: '고객', accessor: '고객',},
+    {Header: '고객연락처', accessor: '고객연락처',},
+    {Header: '시즌', accessor: '시즌',},
+    {Header: '스타일', accessor: '스타일',},
+    {Header: '컬러', accessor: '컬러',},
+    {Header: '사이즈', accessor: '사이즈',},
+    {Header: '판매가', accessor: '판매가',},
+    {Header: '고객요구', accessor: '고객요구',},
+    {Header: '매장접수내용', accessor: '매장접수내용',},
+    {Header: '과실구분', accessor: '과실구분',},
+    {Header: '고객약속일', accessor: '고객약속일',},  
+    {Header: '본사접수일', accessor: '본사접수일',},
+    {Header: '내용분석', accessor: '내용분석',},
+    {Header: '판정결과', accessor: '판정결과',},
+    ],[])
+
+    const value = data.map((pairRepair) => ({
+      // "선택":"V",
+      "서비스카드 번호":pairRepair[RECEIPT.CODE],
+      "매장코드":pairRepair[STORE.CODE],
+      "매장명":pairRepair[STORE.NAME],
+      "매장구분":STORE_CATEGORY[pairRepair[STORE.CATEGORY]],
+      "매장연락처":pairRepair[STORE.CONTACT],
+      "등록일":"?",
+      "고객ID":pairRepair[CUSTOMER.ID],
+      "고객":pairRepair[CUSTOMER.NAME],
+      "고객연락처":pairRepair[CUSTOMER.CONTACT],
+      "시즌":pairRepair[PRODUCT.SEASON],
+      "스타일":pairRepair[PRODUCT.STYLE],
+      "컬러":pairRepair[PRODUCT.COLOR],
+      "사이즈":pairRepair[PRODUCT.SIZE], 
+      "판매가":pairRepair[PRODUCT.PRICE],
+      "고객요구":RECEIPT_TYPE[pairRepair[RECEIPT.TYPE]],
+      "매장접수내용":pairRepair[RECEIPT.STORE_MESSAGE],
+      "과실구분":pairRepair[RECEIPT.FAULT_NAME],
+      "고객약속일":pairRepair[RECEIPT.DUE_DATE]
+      ? moment(pairRepair[RECEIPT.DUE_DATE]).format("YYYY-MM-DD")
+      : "",
+      "본사접수일":pairRepair[RECEIPT.REGISTER_DATE]
+      ? moment(pairRepair[RECEIPT.REGISTER_DATE]).format("YYYY-MM-DD")
+      : "",
+      "내용분석":pairRepair[RECEIPT.ANALYSIS_NAME],
+      "판정결과":pairRepair[RECEIPT.RESULT_NAME],
+    }));
+
   return (
     <Wrapper>
-      <Table>
-        <TableHeader>
-          <tr>
-            <TableHeaderCell width="40px">선택</TableHeaderCell>
-            <TableHeaderCell>No.</TableHeaderCell>
-            <TableHeaderCell>Shop</TableHeaderCell>
-            <TableHeaderCell width="150px">매장명</TableHeaderCell>
-            <TableHeaderCell width="70px">구분</TableHeaderCell>
-            <TableHeaderCell width="150px">매장 연락처</TableHeaderCell>
-            <TableHeaderCell width="120px">등록일</TableHeaderCell>
-            <TableHeaderCell>고객ID</TableHeaderCell>
-            {/* <TableHeaderCell>접수구분</TableHeaderCell> */}
-            <TableHeaderCell>고객</TableHeaderCell>
-            <TableHeaderCell width="150px">연락처</TableHeaderCell>
-            <TableHeaderCell width="70px">시즌</TableHeaderCell>
-            <TableHeaderCell>스타일</TableHeaderCell>
-            {/* <TableHeaderCell width="70px">차수</TableHeaderCell> */}
-            <TableHeaderCell width="70px">컬러</TableHeaderCell>
-            <TableHeaderCell width="70px">사이즈</TableHeaderCell>
-            <TableHeaderCell>판매가</TableHeaderCell>
-            <TableHeaderCell>고객요구</TableHeaderCell>
-            <TableHeaderCell width="150px">접수내용</TableHeaderCell>
-            <TableHeaderCell>과실구분</TableHeaderCell>
-            <TableHeaderCell width="120px">약속일</TableHeaderCell>
-            <TableHeaderCell width="120px">본사접수일</TableHeaderCell>
-            <TableHeaderCell>내용분석</TableHeaderCell>
-            <TableHeaderCell>판정결과</TableHeaderCell>
-          </tr>
-        </TableHeader>
-        <tbody>
-          {data.map((receipt) => (
-            <TableRow key={receipt[RECEIPT.ID]}>
-              <TableData width="40px">
-                <Checkbox value={receipt[RECEIPT.ID]} />
-              </TableData>
-              <TableData>{receipt[RECEIPT.CODE]}</TableData>
-              <TableData>{receipt[STORE.ID]}</TableData>
-              <TableData title={receipt[STORE.NAME]} width="200px">
-                {receipt[STORE.NAME]}
-              </TableData>
-              <TableData width="70px">
-                {STORE_TYPE[receipt[STORE.TYPE]]}
-              </TableData>
-              <TableData width="150px">{receipt[STORE.CONTACT]}</TableData>
-              <TableData width="120px">
-                {receipt[RECEIPT.RECEIPT_DATE]
-                  ? moment(receipt[RECEIPT.RECEIPT_DATE]).format("YYYY-MM-DD")
-                  : ""}
-              </TableData>
-              <TableData>{receipt[CUSTOMER.ID]}</TableData>
-              {/* <TableData>
-                {RECEIPT_CATEGORY_TYPE[receipt[RECEIPT.CATEGORY]]}
-              </TableData> */}
-              <TableData>{receipt[CUSTOMER.NAME]}</TableData>
-              <TableData width="150px">{receipt[CUSTOMER.CONTACT]}</TableData>
-              <TableData width="70px">{receipt[PRODUCT.SEASON]}</TableData>
-              <TableData>{receipt[PRODUCT.STYLE]}</TableData>
-              {/* <TableData width="70px">{receipt[PRODUCT.DEGREE]}</TableData> */}
-              <TableData width="70px">{receipt[PRODUCT.COLOR]}</TableData>
-              <TableData width="70px">{receipt[PRODUCT.SIZE]}</TableData>
-              <TableData>{receipt[PRODUCT.PRICE]}</TableData>
-              <TableData>{RECEIPT_TYPE[receipt[RECEIPT.TYPE]]}</TableData>
-              <TableData width="150px"></TableData>
-              <TableData>{receipt[RECEIPT.FAULT_NAME]}</TableData>
-              <TableData width="120px">
-                {receipt[RECEIPT.DUE_DATE]
-                  ? moment(receipt[RECEIPT.DUE_DATE]).format("YYYY-MM-DD")
-                  : ""}
-              </TableData>
-              <TableData width="120px">
-                {receipt[RECEIPT.REGISTER_DATE]
-                  ? moment(receipt[RECEIPT.REGISTER_DATE]).format("YYYY-MM-DD")
-                  : ""}
-              </TableData>
-              <TableData>{receipt[RECEIPT.ANALYSIS_NAME]}</TableData>
-              <TableData>{receipt[RECEIPT.RESULT_NAME]}</TableData>
-            </TableRow>
-          ))}
-        </tbody>
-      </Table>
+      <Styles>
+        <Table columns={columns} data={value}/>
+      </Styles>
     </Wrapper>
   );
 };
 
+const Styles = styled.div`
+  padding: 1rem;
+
+  .table {
+    display: inline-block;
+    border-spacing: 0;
+    border: 1px solid black;
+
+    .tr {
+      :last-child {
+        .td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    .th,
+    .td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      ${'' /* In this example we use an absolutely position resizer,
+       so this is required. */}
+      position: relative;
+
+      :last-child {
+        border-right: 0;
+      }
+
+      .resizer {
+        display: inline-block;
+        /* background: black; */
+        width: 2px;
+        height: 100%;
+        position: absolute;
+        right: 0;
+        top: 0;
+        transform: translateX(50%);
+        z-index: 1;
+        ${'' /* prevents from scrolling while dragging on touch devices */}
+        touch-action:none;
+      }
+    }
+  }
+`
+
 const Wrapper = styled.div`
+  /* position: absolute; */
   height: 50%;
   width: 100%;
   overflow: scroll;
   border-bottom: 2px solid;
-`;
-
-const Table = styled.table`
-  border-collapse: collapse;
-  min-width: 100%;
 `;
 
 const TableHeader = styled.thead`
@@ -135,4 +237,8 @@ const TableData = styled.td`
   white-space: nowrap;
 `;
 
+
+
 export default ReturnList;
+
+        
