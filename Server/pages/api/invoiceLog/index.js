e11d2888407b4cda37ac29e14b05d47cd2a1e5c0
Gumox <fodhs1000@naver.com>
2022-03-01 13:17:05 +0900

    Update index.js

--- Server/pages/api/invoiceLog/index.js
+++ Server/pages/api/invoiceLog/index.js
@@ -1,7 +1,7 @@
 import { result } from "lodash";
 import excuteQuery from "../db"
 
-'2011-12-18 13:17:17'
+"2011-12-18 13:17:17"
 
 async function returnInvoiceLog(
     receipt_id,
@@ -29,39 +29,114 @@ async function returnInvoiceLog(
     
       return result;
 }
+async function getInvoice(receipt_code){
+  const result = await excuteQuery({
+    query: 
+            `SELECT * FROM invoice WHERE receipt_id =?`,
+    values:[receipt_code],
+  });
+  return result
+}
+async function getIssuedInvoice(receipt_code) {
+  const result = await excuteQuery({
+      query: 
+              `
+              UPDATE
+                  receipt
+              SET
+                  issued = 1
+              WHERE
+              receipt_id  = ?
+              
+              `,
+      values:[receipt_code],
+    });
+  
+    return result;
+}
 
+async function invoice(List,user) {
+  let results=[]
+  for (let data of List) {
+    let qty =1
+    let toDay =  new Date();
+    let inputDate = toDay.getFullYear()+"-"+toDay.getMonth()+"-"+toDay.getDay()+" "+toDay.getHours()+":"+toDay.getMinutes()+":"+toDay.getSeconds();
+    console.log(inputDate);
+    let setIssued = [];
+    let inputInvoiceLog = [];
+    console.log("-------------------------")
+    console.log(data)
+    const checkInvoice = await getInvoice(data.receipt_id)
+    console.log("-----88888--")
+    console.log(checkInvoice.length)
+    console.log("-------------------------")
+    if(checkInvoice.length > 0){
+      if((checkInvoice.length) % 2 === 1){
+        qty = -1;
+      }
+      setIssued = await getIssuedInvoice(data.receipt_id)
+      inputInvoiceLog =await returnInvoiceLog(
+                                                      data["receipt_id"],
+                                                      data["서비스카드 번호"],
+                                                      /*data[""]*/"2020-12-12 12:00:00",//임시 값 추후 변경 필요
+                                                      data["매장구분"],
+                                                      data["시즌"],
+                                                      data["스타일"],
+                                                      data["컬러"],
+                                                      data["사이즈"],
+                                                      qty,
+                                                      "123",
+                                                      checkInvoice[0].created,
+                                                      checkInvoice[0].created_date,
+                                                      user,
+                                                      inputDate)
+                                                      
+      results.push(inputInvoiceLog)
+    }else{
+      console.log("OK")
+      setIssued = await getIssuedInvoice(data.receipt_id)
+      inputInvoiceLog =await returnInvoiceLog(
+                                                      data["receipt_id"],
+                                                      data["서비스카드 번호"],
+                                                      /*data[""]*/"2020-12-12 12:00:00",//임시 값 추후 변경 필요
+                                                      data["매장구분"],
+                                                      data["시즌"],
+                                                      data["스타일"],
+                                                      data["컬러"],
+                                                      data["사이즈"],
+                                                      qty,
+                                                      "123",
+                                                      user,
+                                                      inputDate,
+                                                      user,
+                                                      inputDate)
+                                                      
+      results.push(inputInvoiceLog)
+    }
+  }
+  return results;
+}
 const controller = async (req, res) => {
     if (req.method === "PUT") {
         console.log("req.headers.referer");
         console.log(req.headers.referer);
         console.log("req.query");
-        console.log(req.query);
+        console.log(req.body);
         
         const {
-            receipt_id, 
-            receipt_code, 
-            release_date, 
-            status, 
-            season, 
-            partcode, 
-            color,
-            size,
-            qty,
-            amount,
-            created,
-            created_date, 
-            edited,
-            edited_date
-        } = req.query;
+            list,
+            user
+        } = req.body;
         
 
     try {
-      const result = await returnInvoiceLog(receipt_id, receipt_code, release_date, status, season, partcode, color, size, qty, amount, created, created_date, edited, edited_date);
-      console.log(result);
-      if (result.error) throw new Error(result.error);
-      if (result.length) {
-        console.log("result");
-        res.status(200).json({ data: result });
+      //const result = await returnInvoiceLog(receipt_id, receipt_code, release_date, status, season, partcode, color, size, qty, amount, created, created_date, edited, edited_date);
+      const result = invoice(list,user)
+      //console.log(result);
+      if (result.error) throw new Error(result.error); 
+      //console.log(result)
+      if (result ) {
+        res.status(200).json({ send: true });
       } else {
         console.log("No result");
         res.status(204).json({ message: "No result" });
