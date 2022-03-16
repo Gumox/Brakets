762d636dc34af867b2ee11fe12bcbe491c7d15ec
Gumox <fodhs1000@naver.com>
2022-03-16 10:46:54 +0900

    Update SettlementResult.js

--- Server/components/repair/SettlementResult.js
+++ Server/components/repair/SettlementResult.js
@@ -103,7 +103,7 @@ const SettlementResult =(props)=>{
         return ()=>{
             window.removeEventListener('resize',handleResize);
         }
-    })
+    },[item, handleResize, types])
     return(
         
            <LaView><Container>
