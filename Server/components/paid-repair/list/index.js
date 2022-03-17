fbc40a31a27a257a934f3ef6e7a96ea8b3e7e2d1
Jwon <check4545@daum.net>
2022-03-17 14:42:51 +0900

    fix

--- Server/components/paid-repair/list/index.js
+++ Server/components/paid-repair/list/index.js
@@ -159,14 +159,36 @@ const ReturnList = ({ data, user }) => {
     // 단순 조회 기능 -> Check Box 필요 없을 듯?
     // {Header: '선택',  accessor: '선택',},
 
+    // {Header: '서비스카드 번호', accessor: '서비스카드 번호',},
+    // {Header: '매장코드',   accessor: '매장코드',},
+    // {Header: '매장명', accessor: '매장명',},
+    // {Header: '매장구분', accessor: '매장구분',},
+    // {Header: '매장연락처', accessor: '매장연락처',},
+    // {Header: '등록일', accessor: '등록일',},
+    // {Header: '고객ID', accessor: '고객ID',},
+    // {Header: '고객', accessor: '고객',},
+    // {Header: '고객연락처', accessor: '고객연락처',},
+    // {Header: '시즌', accessor: '시즌',},
+    // {Header: '스타일', accessor: '스타일',},
+    // {Header: '컬러', accessor: '컬러',},
+    // {Header: '사이즈', accessor: '사이즈',},
+    // {Header: '판매가', accessor: '판매가',},
+    // {Header: '고객요구', accessor: '고객요구',},
+    // {Header: '매장접수내용', accessor: '매장접수내용',},
+    // {Header: '과실구분', accessor: '과실구분',},
+    // {Header: '고객약속일', accessor: '고객약속일',},  
+    // {Header: '본사접수일', accessor: '본사접수일',},
+    // {Header: '내용분석', accessor: '내용분석',},
+    // {Header: '판정결과', accessor: '판정결과',},
+
     {Header: '서비스카드 번호', accessor: '서비스카드 번호',},
-    {Header: '매장코드',   accessor: '매장코드',},
+    {Header: '매장코드', accessor: '매장코드',},
     {Header: '매장명', accessor: '매장명',},
     {Header: '매장구분', accessor: '매장구분',},
-    {Header: '매장연락처', accessor: '매장연락처',},
+    {Header: '매장 연락처', accessor: '매장 연락처',},
     {Header: '등록일', accessor: '등록일',},
-    {Header: '고객ID', accessor: '고객ID',},
-    {Header: '고객', accessor: '고객',},
+    {Header: '고객 ID', accessor: '고객 ID',},
+    {Header: '고객 ', accessor: '고객 ',},
     {Header: '고객연락처', accessor: '고객연락처',},
     {Header: '시즌', accessor: '시즌',},
     {Header: '스타일', accessor: '스타일',},
@@ -175,22 +197,51 @@ const ReturnList = ({ data, user }) => {
     {Header: '판매가', accessor: '판매가',},
     {Header: '고객요구', accessor: '고객요구',},
     {Header: '매장접수내용', accessor: '매장접수내용',},
-    {Header: '과실구분', accessor: '과실구분',},
-    {Header: '고객약속일', accessor: '고객약속일',},  
+    {Header: '고객약속일', accessor: '고객약속일',},
     {Header: '본사접수일', accessor: '본사접수일',},
+    {Header: '발송일 to S', accessor: '발송일 to S',},
+    {Header: '과실구분', accessor: '과실구분',},
     {Header: '내용분석', accessor: '내용분석',},
     {Header: '판정결과', accessor: '판정결과',},
+    {Header: '수선미입고', accessor: '수선미입고',},
+    {Header: '유상수선비', accessor: '유상수선비',},
+    {Header: '유상수선 유/무', accessor: '유상수선 유/무',},
+    {Header: '수선처1', accessor: '수선처1',},
+    {Header: '총수선비1', accessor: '총수선비1',},
+    {Header: '수선처접수일1', accessor: '수선처접수일1',},
+    {Header: '재수선1', accessor: '재수선1',},
+    {Header: '수선처2', accessor: '수선처2',},
+    {Header: '총수선비2', accessor: '총수선비2',},
+    {Header: '수선처접수일2', accessor: '수선처접수일2',},
+    {Header: '재수선2', accessor: '재수선2',},
+    {Header: '수선처3', accessor: '수선처3',},
+    {Header: '총수선비3', accessor: '총수선비3',},
+    {Header: '수선처접수일3', accessor: '수선처접수일3',},
+    {Header: '재수선3', accessor: '재수선3',},
+    {Header: '생산업체', accessor: '생산업체',},
+    {Header: '발송일 to M', accessor: '발송일 to M',},
+    {Header: '본사 설명', accessor: '본사 설명',},
+    {Header: '하자코드', accessor: '하자코드',},
+    {Header: '심의', accessor: '심의',},
+    {Header: '등록자', accessor: '등록자',},
+    {Header: '등록일시', accessor: '등록일시',},
+    {Header: '수정자', accessor: '수정자',},
+    {Header: '수정일시', accessor: '수정일시',},
+    {Header: 'SMS', accessor: 'SMS',},
     ],[])
 
+
+    console.log("data is")
+    console.log(data)
     const value = data.map((pairRepair) => ({
-      
       "receipt_id":pairRepair[RECEIPT.ID],
       "서비스카드 번호":pairRepair[RECEIPT.CODE],
       "매장코드":pairRepair[STORE.CODE],
       "매장명":pairRepair[STORE.NAME],
       "매장구분":STORE_CATEGORY[pairRepair[STORE.CATEGORY]],
       "매장연락처":pairRepair[STORE.CONTACT],
-      "등록일":"?",
+      "등록일":pairRepair.receipt_date ? moment(pairRepair.receipt_date).format("YYYY-MM-DD")
+      : "",
       "고객ID":pairRepair[CUSTOMER.ID],
       "고객":pairRepair[CUSTOMER.NAME],
       "고객연락처":pairRepair[CUSTOMER.CONTACT],
@@ -201,15 +252,43 @@ const ReturnList = ({ data, user }) => {
       "판매가":pairRepair[PRODUCT.PRICE],
       "고객요구":RECEIPT_TYPE[pairRepair[RECEIPT.TYPE]],
       "매장접수내용":pairRepair[RECEIPT.STORE_MESSAGE],
-      "과실구분":pairRepair[RECEIPT.FAULT_NAME],
       "고객약속일":pairRepair[RECEIPT.DUE_DATE]
       ? moment(pairRepair[RECEIPT.DUE_DATE]).format("YYYY-MM-DD")
       : "",
       "본사접수일":pairRepair[RECEIPT.REGISTER_DATE]
       ? moment(pairRepair[RECEIPT.REGISTER_DATE]).format("YYYY-MM-DD")
       : "",
+      "발송일 to S":pairRepair.complete_date ? moment(pairRepair.complete_date).format("YYYY-MM-DD")
+      : "",
+      "과실구분":pairRepair[RECEIPT.FAULT_NAME],
       "내용분석":pairRepair[RECEIPT.ANALYSIS_NAME],
-      "판정결과":pairRepair[RECEIPT.RESULT_NAME],
+      "판정결과":pairRepair[RECEIPT.RESULT_NAME],      
+      "수선미입고":pairRepair[RECEIPT.RESULT_DETAIL_NAME],
+      "유상수선비":pairRepair.fee,
+      "유상수선 유/무":(pairRepair.fee > 0) ? "유" : "무",
+      "수선처1":"",
+      "총수선비1":"",
+      "수선처접수일1":"",
+      "재수선1":"",
+      "수선처2":"",
+      "총수선비2":"",
+      "수선처접수일2":"",
+      "재수선2":"",
+      "수선처3":"",
+      "총수선비3":"",
+      "수선처접수일3":"",
+      "재수선3":"",
+      "생산업체":pairRepair.manufacturer_name,
+      "발송일 to M":"",
+      "본사 설명":pairRepair.message,
+      "하자코드":"",
+      "심의":pairRepair[RECEIPT.TYPE],
+
+      "등록자":"",
+      "등록일시":"",
+      "수정자":"",
+      "수정일시":"",
+      "SMS":"",
     }));
 
   return (
@@ -275,7 +354,7 @@ const Styles = styled.div`
 
 const Wrapper = styled.div`
   /* position: absolute; */
-  height: 95%;/70%
+  height: 95%;
   width: 100%;
   overflow: scroll;
   border-bottom: 2px solid;
