fccabc93e9e6fca1a5a1e1797d4ec5875eed43b9
Gumox <fodhs1000@naver.com>
2022-03-18 13:15:42 +0900

    Update RepairReception.js

--- Server/pages/RepairReception.js
+++ Server/pages/RepairReception.js
@@ -69,31 +69,6 @@ function RepairReception({options,user}) {
       }
     })
   }
-  const postRepairNeedPoint = async () => {
-    let formdata = new FormData();
-
-    formdata.append("receipt", 117);
-    formdata.append("store", 12);
-    
-    console.log(formdata)
-    
-    try {
-        const response = await fetch(`${process.env.API_URL}/needRepair`,{method: 'POST',
-        headers: {
-            'Accept': '',
-            },
-        body: formdata
-        });
-        const json = await response.json();
-        console.log(json)
-        navigation.popToTop();
-        navigation.navigate("PhotoStep")
-    } catch (error) {
-        console.error(error);
-    } finally {
-
-    } 
-}
   useEffect(()=>{
     localStorage.setItem('COMPANY',JSON.stringify(selectItems));
     localStorage.setItem('SHOP',shop_id)
@@ -108,7 +83,6 @@ function RepairReception({options,user}) {
         <TopView>
               <h2>접수</h2>
 
-              <button onClick={()=>{postRepairNeedPoint()}}>0000000</button>
               <Image alt="excel" src='/icons/excel.png' width={45} height={40} onClick={()=>{getExcel(parseRepairReceptionData(listData))}}/>
               
         </TopView>
