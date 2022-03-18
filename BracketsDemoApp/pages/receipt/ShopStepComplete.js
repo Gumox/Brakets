dcade68df83b246b6f2c2d04c5176602828c2152
Jwon <check4545@daum.net>
2022-03-18 17:18:12 +0900

    fix page layout

--- BracketsDemoApp/pages/receipt/ShopStepComplete.js
+++ BracketsDemoApp/pages/receipt/ShopStepComplete.js
@@ -8,11 +8,13 @@ import StateBarSolid from '../../components/StateBarSolid';
 import TopInfo from '../../components/TopInfo';
 import Bottom from '../../components/Bottom'
 import store from '../../store/store';
-import { Modal, StyleSheet, View, Pressable,
-        Image, Text, ScrollView } from 'react-native';
+import {
+    Modal, StyleSheet, View, Pressable,
+    Image, Text, ScrollView
+} from 'react-native';
 import ImageZoom from 'react-native-image-pan-zoom';
 import ip from '../../serverIp/Ip';
-import {useNetInfo}from "@react-native-community/netinfo";
+import { useNetInfo } from "@react-native-community/netinfo";
 
 
 const Label = styled.Text`
@@ -23,7 +25,7 @@ const Label = styled.Text`
     margin-left:12px;
 `;
 
-const RegistText= styled.Text`
+const RegistText = styled.Text`
     font-weight: bold;
     font-size: 30px;
     color:#000000;
@@ -54,7 +56,7 @@ const InnerView = styled.View`
     align-items: center;
     justify-content: center;
 `;
-const InfoView =styled.View`
+const InfoView = styled.View`
     width: 70%;
     border:2px solid  #78909c;
     border-radius:12px;
@@ -77,7 +79,7 @@ const TouchableView = styled.TouchableOpacity`
     background-color:#d6d6d6;
     border-radius:10px;
 `;
-const ImgIcon =styled.Image`
+const ImgIcon = styled.Image`
     width: 25px;
     height: 20px;
     margin-right:10px;
@@ -95,61 +97,61 @@ const CodeView = styled.View`
 `;
 
 const styles = StyleSheet.create({
-        centeredView: {
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: 22
-        },
-        xView:{
-            backgroundColor: "#78909c",
-            borderRadius: 20,
-        },
-        modalView: {
-          margin: 10,
-          backgroundColor: "white",
-          borderRadius: 20,
-          paddingRight: 5,
-          paddingLeft: 5,
-          paddingTop: 8,
-          paddingBottom: 8,
-          alignItems: "center",
-          shadowColor: "#000",
-          shadowOffset: {
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+    },
+    xView: {
+        backgroundColor: "#78909c",
+        borderRadius: 20,
+    },
+    modalView: {
+        margin: 10,
+        backgroundColor: "white",
+        borderRadius: 20,
+        paddingRight: 5,
+        paddingLeft: 5,
+        paddingTop: 8,
+        paddingBottom: 8,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
             width: 0,
             height: 2
-          },
         },
-        ViewCenter:{
-            justifyContent: "center",
-            alignItems: "center",
-        }
-      });
+    },
+    ViewCenter: {
+        justifyContent: "center",
+        alignItems: "center",
+    }
+});
 // 구조 분해 할당, Destructuring Assignment
-function ShopStepOne( { navigation } ) {
+function ShopStepOne({ navigation }) {
     const [barcode, setBarcode] = React.useState(store.getState().cardValue);
     const [bag, setBag] = React.useState(store.getState().bagCodeValue);
-    
+
 
     const cardImgUri = store.getState().card;
-    const bagImgUri =store.getState().bagPicture;
+    const bagImgUri = store.getState().bagPicture;
 
 
     const [modalVisible, setModalVisible] = React.useState(false);
     const [modalVisible2, setModalVisible2] = React.useState(false);
 
-    const sendForRepir =store.getState().basicRepairStore;
-    const [repairShop,setRepairShop] =React.useState(null);
-    
+    const sendForRepir = store.getState().basicRepairStore;
+    const [repairShop, setRepairShop] = React.useState(null);
 
-    
+
+
     const netInfo = useNetInfo();
-    
-    React.useEffect(()=>{
+
+    React.useEffect(() => {
         setRepairShop(sendForRepir)
-    },[]);
-    
-    const submitReceipt = async (receipt_id,bag_code) => {
+    }, []);
+
+    const submitReceipt = async (receipt_id, bag_code) => {
         var formdata = new FormData();
 
         formdata.append("receipt", receipt_id);
@@ -157,23 +159,24 @@ function ShopStepOne( { navigation } ) {
         console.log(formdata)
 
         try {
-            const response = await fetch(ip+'/api/submitReceipt',{method: 'POST',
-            headers: {
-                'Accept': '',
-                'Content-Type': 'multipart/form-data'
+            const response = await fetch(ip + '/api/submitReceipt', {
+                method: 'POST',
+                headers: {
+                    'Accept': '',
+                    'Content-Type': 'multipart/form-data'
                 },
-            body: formdata
+                body: formdata
             });
             const json = await response.json();
             console.log(json)
-           
+
         } catch (error) {
             console.error(error);
         } finally {
 
         }
     }
-    
+
 
 
     return (
@@ -186,11 +189,11 @@ function ShopStepOne( { navigation } ) {
                     <RegistText>접수 완료</RegistText>
                     <PrView><BlueText>완료</BlueText><GrayText> 버튼을 누르시면</GrayText></PrView>
                     <PrView><BlueText>수선 접수</BlueText><GrayText> 가</GrayText><BlueText> 완료</BlueText><GrayText> 됩니다</GrayText></PrView>
+
                 </CenterText>
                 <InnerView>
                     <InfoView>
                         <GrayText>서비스 카드 번호</GrayText>
-
                         <Modal
                             animationType="slide"
                             transparent={true}
@@ -207,20 +210,15 @@ function ShopStepOne( { navigation } ) {
                                             cropHeight={400}
                                             imageWidth={300}
                                             imageHeight={400}>
-                                            {/* <Image style={{width:300, height:400}}
-                                    source={{uri:cardImgUri}}/> */}
+                                            <Image style={{ width: 300, height: 400 }}
+                                                source={{ uri: cardImgUri }} />
                                         </ImageZoom>
 
                                     </View>
                                 </View>
                             </View>
                         </Modal>
-
-
-                        <TouchableView onPress={() => setModalVisible(!modalVisible)}>
-                            <Label>{barcode}</Label>
-                            {/* <ImgIcon source={require('../../Icons/image.png')}/> */}
-                        </TouchableView>
+                        <TouchableView onPress={() => setModalVisible(!modalVisible)}><Label>{barcode}</Label><ImgIcon source={require('../../Icons/image.png')} /></TouchableView>
 
                         <GrayText>받는 곳</GrayText>
                         <DataView><Label>{repairShop}</Label></DataView>
@@ -241,21 +239,17 @@ function ShopStepOne( { navigation } ) {
                                             cropHeight={400}
                                             imageWidth={300}
                                             imageHeight={400}>
-                                            {/* <Image style={{width:300, height:400}}
-                                    source={{uri:bagImgUri}}/> */}
+                                            <Image style={{ width: 300, height: 400 }}
+                                                source={{ uri: bagImgUri }} />
                                         </ImageZoom>
 
                                     </View>
                                 </View>
                             </View>
                         </Modal>
-                        <TouchableView onPress={() => setModalVisible2(!modalVisible)}>
-                            <Label>{bag}</Label>
-                            {/* <ImgIcon source={require('../../Icons/image.png')}/> */}
-                        </TouchableView>
+                        <TouchableView onPress={() => setModalVisible2(!modalVisible)}><Label>{bag}</Label><ImgIcon source={require('../../Icons/image.png')} /></TouchableView>
 
                     </InfoView>
-
                     <Button onPress={() => {
                         if (netInfo.isConnected) {
                             submitReceipt(store.getState().receipt_id, bag)
@@ -264,7 +258,6 @@ function ShopStepOne( { navigation } ) {
                         } else {
                             alert("네트워크 연결 실패\n 연결 상태를 확인해주세요")
                         }
-
                     }}>
                         완료
                     </Button>
@@ -272,6 +265,7 @@ function ShopStepOne( { navigation } ) {
             </ScrollView>
             <Bottom navigation={navigation} />
         </>
+
     )
 }
 
