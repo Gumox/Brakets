31a14a090c0e80f08a59a1b20a3c7a566ec3c2d3
Gumox <fodhs1000@naver.com>
2022-03-31 12:08:37 +0900

    ui 수정

--- BracketsDemoApp/components/TopInfo.js
+++ BracketsDemoApp/components/TopInfo.js
@@ -4,13 +4,19 @@ import store from '../store/store';
 
 function TopInfo(props) {
     const cName = store.getState().customer.cName
+    let line;
+    if(cName){
+        line =(<View style={{flexDirection:"row"}}><Text style ={{color:"#000000",fontWeight:"bold"}}>{cName}</Text><Text style={{color:"#000000"}}> 님 진행중</Text></View>)
+    }else{
+        line =(<View style={{flexDirection:"row"}}><Text style ={{color:"#000000",fontWeight:"bold"}}></Text><Text style={{color:"#000000"}}>　　　　　</Text></View>)
+    }
     return (
         <View style={{width:'100%',flexDirection:"row",justifyContent:"space-around",marginBottom:10}}>
             <View style={{flexDirection:"row"}}><Text style={{color:"#000000",fontWeight: "bold",fontSize:15}}>{store.getState().receptionDivision.name}</Text><Text  style={{color:"#000000",fontWeight: "bold",fontSize:15}}> : </Text>
                 <Text  style={{color:"#000000",fontWeight: "bold",fontSize:15}}>{store.getState().requirement.name}</Text>
             </View>
             <Text>  </Text>
-            {cName&&(<View style={{flexDirection:"row"}}><Text style ={{color:"#000000",fontWeight:"bold"}}>{cName}</Text><Text style={{color:"#000000"}}> 님 진행중</Text></View>)}
+            {line}
         </View>
     )
 } 
--- BracketsDemoApp/components/TopInfoLess.js
+++ BracketsDemoApp/components/TopInfoLess.js
@@ -3,13 +3,21 @@ import { View,Text } from 'react-native';
 import store from '../store/store';
 
 function TopInfoLess(props) {
+    
+    const cName = store.getState().customer.cName
+    let line;
+    if(cName){
+        line =(<View style={{flexDirection:"row"}}><Text style ={{color:"#000000",fontWeight:"bold"}}>{cName}</Text><Text style={{color:"#000000"}}> 님 진행중</Text></View>)
+    }else{
+        line =(<View style={{flexDirection:"row"}}><Text style ={{color:"#000000",fontWeight:"bold"}}></Text><Text style={{color:"#000000"}}>　　　　　</Text></View>)
+    }
     return (
         <View style={{width:'100%',flexDirection:"row",justifyContent:"space-around",marginBottom:10}}>
             <View style={{flexDirection:"row"}}><Text style={{color:"#000000",fontWeight: "bold",fontSize:15}}>{store.getState().receptionDivision.name}</Text>
                 
             </View>
             <Text>  </Text>
-            <View style={{flexDirection:"row"}}><Text style ={{color:"#000000",fontWeight:"bold"}}>{store.getState().customer.cName}</Text><Text style={{color:"#000000"}}> 님 진행중</Text></View>
+           {line}
         </View>
     )
 }
