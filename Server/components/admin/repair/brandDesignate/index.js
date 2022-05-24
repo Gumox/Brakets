9565e19229c63338d9d336e68ab12f9d283948e7
Gumox <fodhs1000@naver.com>
2022-05-24 15:04:55 +0900

    Update index.js

--- Server/components/admin/repair/brandDesignate/index.js
+++ Server/components/admin/repair/brandDesignate/index.js
@@ -98,94 +98,98 @@ const BrandDesignate = ({user,infos,store,categorys,repairShops}) => {
     return(
         <Wrapper>
             <div>
-              <SearchBar style={{width:"750px"}}>
-                <SearchBarHeader >
-                    조회 조건
-                </SearchBarHeader>
-                    <PrView style={{flex:1.5, backgroundColor:COLOR.WHITE}}>
-                        <PrView style={{flex:1, backgroundColor:COLOR.WHITE,borderRadius:0,}}>
-                            
-                            <SelectItemHeader >
-                                브랜드
-                            </SelectItemHeader>
-                            <SearchSelect value={selectedBrand} style={{paddingLeft:20,flex:0.7,borderLeft:0,borderRight:0,borderTop:`2px solid ${COLOR.LIGHT_GRAY}`,borderBottom:`2px solid ${COLOR.LIGHT_GRAY}`}} 
-                                onChange={(e)=>{setSelectedBrand(e.target.value)}}>
-                                {
-                                    categorysBrand.map((item,index)=>(
-                                        <option key={index} value={item.brand_id} >{item.brand_name}</option>
-                                    ))
-                                }
-                            </SearchSelect>
-
-                            <SelectItemHeader >
-                                카테고리
-                            </SelectItemHeader>
-                            <SearchSelect value={selectedCategory} style={{paddingLeft:20,flex:0.7,borderLeft:0,borderRight:0,borderTop:`2px solid ${COLOR.LIGHT_GRAY}`,borderBottom:`2px solid ${COLOR.LIGHT_GRAY}`}} 
-                                onChange={(e)=>{setSelectedCategory(e.target.value)}}>
-                                <option value={"ALL"} >{"전체"}</option>
-                                {
-                                    brandCategoryList.map((item,index)=>(
-                                        <option key={index} value={item.pcategory_id} >{item.category_name}</option>
-                                    ))
-                                }
-                            </SearchSelect>
-                          
-                      </PrView> 
-                    </PrView>
-
-                    <SearchBarButton onClick={()=>{
-                        searchHandler(selectedBrand)
-                    }}>
-                    <SearchImage  src="/icons/search.png"/>
-                    
-                    </SearchBarButton>
-                </SearchBar>
-            
-                <h2 style={{marginLeft:20}}>검색 결과</h2>
-                <LaView style={{justifyContent:"end",width:"550px"}}>
-                    <DivButton onClick={()=>{setDesignateOn(true)}}>추가</DivButton>
-                    <DivButton style={{color:COLOR.RED,marginLeft:20}} onClick={()=>{setDeassignOn(true)}}>삭제</DivButton>
-                </LaView>
-                <InputTableBox style={{width:"550px"}}>
-                    {
-                        searchResult.length > 0 ?
-                        <div>
-                            <PrView>
-                                <HeaderCell style={{flex:0,minWidth:40,marginLeft:"2px"}}>
-                                    <input type={"checkbox"} checked={allCheck} onChange={()=>{allCheckHandler(!allCheck)}}/>
-                                </HeaderCell>
-                                <HeaderCell >
-                                    카테고리
-                                </HeaderCell>
-
-                                <HeaderCell>
-                                    수선처
-                                </HeaderCell>
-
+                <SearchBar style={{width:"750px"}}>
+                    <SearchBarHeader >
+                        조회 조건
+                    </SearchBarHeader>
+                        <PrView style={{flex:1.5, backgroundColor:COLOR.WHITE}}>
+                            <PrView style={{flex:1, backgroundColor:COLOR.WHITE,borderRadius:0,}}>
                                 
-                            </PrView>
+                                <SelectItemHeader >
+                                    브랜드
+                                </SelectItemHeader>
+                                <SearchSelect value={selectedBrand} style={{paddingLeft:20,flex:0.7,borderLeft:0,borderRight:0,borderTop:`2px solid ${COLOR.LIGHT_GRAY}`,borderBottom:`2px solid ${COLOR.LIGHT_GRAY}`}} 
+                                    onChange={(e)=>{setSelectedBrand(e.target.value)}}>
+                                    {
+                                        categorysBrand.map((item,index)=>(
+                                            <option key={index} value={item.brand_id} >{item.brand_name}</option>
+                                        ))
+                                    }
+                                </SearchSelect>
+
+                                <SelectItemHeader >
+                                    카테고리
+                                </SelectItemHeader>
+                                <SearchSelect value={selectedCategory} style={{paddingLeft:20,flex:0.7,borderLeft:0,borderRight:0,borderTop:`2px solid ${COLOR.LIGHT_GRAY}`,borderBottom:`2px solid ${COLOR.LIGHT_GRAY}`}} 
+                                    onChange={(e)=>{setSelectedCategory(e.target.value)}}>
+                                    <option value={"ALL"} >{"전체"}</option>
+                                    {
+                                        brandCategoryList.map((item,index)=>(
+                                            <option key={index} value={item.pcategory_id} >{item.category_name}</option>
+                                        ))
+                                    }
+                                </SearchSelect>
                             
-                            <List infos={infos} user={user} categorys={searchResult} allCheck={allCheck} setAllCheck={setAllCheck} checkedList={checkedList} setCheckedList={setCheckedList}/>
-                        </div>
-                        :
-                        <PrView style={{backgroundColor:COLOR.WHITE,justifyContent:"center"}}>
-                            결과 없음
+                        </PrView> 
                         </PrView>
+
+                        <SearchBarButton onClick={()=>{
+                            searchHandler(selectedBrand)
+                        }}>
+                        <SearchImage  src="/icons/search.png"/>
+                        
+                        </SearchBarButton>
+                </SearchBar>
+                    
+                <div  style={{display:"flex",flexDirection:"row" , position:"relative"}}>
+
+                    <div style={{width:"555px"}}>
+                        <h2 style={{marginLeft:20}}>검색 결과</h2>
+                        <LaView style={{justifyContent:"end",width:"550px"}}>
+                            <DivButton onClick={()=>{setDesignateOn(true)}}>추가</DivButton>
+                            <DivButton style={{color:COLOR.RED,marginLeft:20}} onClick={()=>{setDeassignOn(true)}}>삭제</DivButton>
+                        </LaView>
+                        <InputTableBox style={{width:"550px"}}>
+                            {
+                                searchResult.length > 0 ?
+                                <div>
+                                    <PrView>
+                                        <HeaderCell style={{flex:0,minWidth:40,marginLeft:"2px"}}>
+                                            <input type={"checkbox"} checked={allCheck} onChange={()=>{allCheckHandler(!allCheck)}}/>
+                                        </HeaderCell>
+                                        <HeaderCell >
+                                            카테고리
+                                        </HeaderCell>
+
+                                        <HeaderCell>
+                                            수선처
+                                        </HeaderCell>
+
+                                        
+                                    </PrView>
+                                    
+                                    <List infos={infos} user={user} categorys={searchResult} allCheck={allCheck} setAllCheck={setAllCheck} checkedList={checkedList} setCheckedList={setCheckedList}/>
+                                </div>
+                                :
+                                <PrView style={{backgroundColor:COLOR.WHITE,justifyContent:"center"}}>
+                                    결과 없음
+                                </PrView>
+                            }
+                        </InputTableBox>
+                    </div>
+
+                    {designateOn &&
+                        <div style={{marginTop:105,marginLeft:20,position:"sticky",top :30}}>
+                            <Designate setDesignateOn={setDesignateOn} selectedBrand={selectedBrand} 
+                                    selectedCategory={selectedCategory} categorysBrand={categorysBrand} 
+                                    brandCategoryList={brandCategoryList} repairShops={repairShops}/>
+                        </div>
                     }
 
-                  {/*
-
-                  
-                   */}
+                </div>
                     
-                  
-              </InputTableBox>
             </div>
-            {designateOn &&
-                <Designate  setDesignateOn={setDesignateOn} selectedBrand={selectedBrand} 
-                            selectedCategory={selectedCategory} categorysBrand={categorysBrand} 
-                            brandCategoryList={brandCategoryList} repairShops={repairShops}/>
-            }
+                
             {deassignOn &&
                 <Deassign setDeassignOn={setDeassignOn} checkedList={checkedList} brandName={selectedBrandName}/>
             }
