0fb5e973abb008805a82528a66dc045240b47277
Gumox <fodhs1000@naver.com>
2022-07-27 16:59:57 +0900

    Update itemCount.js

--- Server/components/repair/receiptionHistory/itemCount.js
+++ Server/components/repair/receiptionHistory/itemCount.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 export default function ItemCount(props){
     const key  =  props.index;
     const count1 = props.count1;
@@ -7,18 +7,29 @@ export default function ItemCount(props){
 
    
 
-    let inItemCount;
-    if(key+1 === 1){
-        inItemCount = count1
-    }else if(key+1 === 2){
-        inItemCount = count2
-    }else if(key+1 === 3){
-        inItemCount = count3
-    }
+    const [inItemCount,setInItemCount] = useState();
+    const [isBlurToggle,setIsBlurToggle] = useState(false);
+    
+    useEffect(()=>{
+        if(key+1 === 1){
+            setInItemCount(count1)
+        }else if(key+1 === 2){
+            setInItemCount(count2)
+        }else if(key+1 === 3){
+            setInItemCount(count3)
+        }
+    },[isBlurToggle])
+    
     return(
-        <input type="number" min="0" style={{width:50,borderWidth:0,borderBottomWidth:2}} value={inItemCount} 
+        <input style={{width:50,borderWidth:0,borderBottomWidth:2}} value={inItemCount} 
+            onFocus={()=>{
+                setInItemCount("")}}
+            onBlur={()=>{
+                setIsBlurToggle(!isBlurToggle)}}
             onChange={(e)=>{
-                inItemCount = e.target.value;
+                let value = e.target.value.replace(/[^0-9]/g,"")
+                e.target.value =value
+                setInItemCount(e.target.value);
                 props.onChange(e)
             }}/>
     )
